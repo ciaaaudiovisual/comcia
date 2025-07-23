85ba21f2083ba0d6343e3d91e2d68bbb578913da
ciaaaudiovisual <133988963+ciaaaudiovisual@users.noreply.github.com>
2025-07-23 16:08:30 -0300

    Update config.py

--- config.py
+++ config.py
@@ -22,6 +22,8 @@ FEATURES_LIST = [
     ('pode_editar_lancamento_faia', 'Pode Editar Lançamentos na Fila', 'admin,supervisor'),
     ('acesso_pagina_revisao_geral', 'Acesso à Página de Revisão Geral', 'admin'),
     ('acesso_pagina_geracao_documentos', 'Acesso à Página de Geração de Documentos', 'admin'),
+    ('acesso_pagina_rancho_pernoite', 'Acesso aos Relatórios de Rancho', 'admin,supervisor,comcia,ajosca'), # <-- ADICIONE ESTA LINHA
+]
 ]
 
 
